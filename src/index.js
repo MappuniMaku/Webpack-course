bbc240850e60a5460eba3f2862f7e13e11aaada0
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-05 00:02:00 +1000

    Installed file-loader in order to be able to work with images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Post from './Post';
-import './styles/styles.css'
+import Happiness from '.assets/happiness.png';
+import './styles/styles.css';
 
-const post = new Post('Webpack Post Title');
+const post = new Post('Webpack Post Title', Happiness);
 
 console.log('Post to string: ', post.toString());
