20f67fd1aa4f0a4767a5e4a1378f86bb3b607914
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-05 02:09:53 +1000

    Imported jQuery into index.js and used it in index.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import * as $ from 'jquery';
 import Post from '@models/Post';
 import Happiness from '@/assets/happiness.png';
 import './styles/styles.css';
 
 const post = new Post('Webpack Post Title', Happiness);
 
-console.log('Post to string: ', post.toString());
+$('pre').html(post.toString());
