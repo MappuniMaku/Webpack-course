21ac8d296d36bacd59d83faaa5f15561dd19812b
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-04 23:21:22 +1000

    Added CleanWebpackPlugin into config, now prev versions of files in dist/ are deleted automatically

--- src/analytics.js
+++ src/analytics.js
@@ -1,6 +1,8 @@
 function createAnalytics() {
     let counter = 0;
-    let isDestroyed = false;
+    let destroyed = false;
+
+    console.log('test');
 
     const listener = () => counter++;
 
@@ -9,11 +11,11 @@ function createAnalytics() {
     return {
         destroy() {
             document.removeEventListener('click', listener);
-            isDestroyed = true;
+            destroyed = true;
         },
 
         getClicks() {
-            if(isDestroyed) {
+            if(destroyed) {
                 return `Analytics is destroyed. Total clicks = ${counter}`;
             };
 
